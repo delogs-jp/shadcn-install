7c42991987ae6d0fc72ef4b12da5af29921214a7
delogs-jp <matsumoto@delogs.jp>
2025-07-09 15:35:43 +0900

    feat: Shadcnのテキストボックスを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8">
-      <h1 className="mb-4 text-2xl font-bold">Shadcn/uiボタンの表示テスト</h1>
+      <h1 className="mb-4 text-2xl font-bold">
+        Shadcn/uiボタンとテキストボックスの表示テスト
+      </h1>
+      <Input type="text" placeholder="お名前" className="mb-4 w-40" />
       <Button className="cursor-pointer font-semibold">クリックしてね</Button>
     </main>
   );
